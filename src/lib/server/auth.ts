0f29913740586c058c17695d50ec14dbc12928b4
Neville Brem <contact@nevillebrem.com>
2025-04-15 20:05:55 +0200

    added hooks and global auth types

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -8,7 +8,10 @@ export const auth = betterAuth({
 		provider: 'pg'
 	}),
 	appName: 'draggon',
+	secret: process.env.AUTH_SECRET!,
 	plugins: [admin(), username()]
 });
 
 export type Auth = typeof auth;
+export type User = typeof auth.$Infer.Session.user;
+export type Session = typeof auth.$Infer.Session.session;
