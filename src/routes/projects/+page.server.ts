d3ed0b79510728d5fef71dd5f64099ad598e43be
Neville Brem <contact@nevillebrem.com>
2025-04-16 12:06:27 +0200

    add modal

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,6 +1,9 @@
 import { requireUser } from '$lib/server/utils';
+import { superValidate } from 'sveltekit-superforms';
+import { arktype } from 'sveltekit-superforms/adapters';
 import type { PageServerLoad } from './$types';
 import { db } from '$lib/server/db';
+import { newProject } from '$lib/arktype';
 
 export const load: PageServerLoad = async () => {
 	const user = requireUser();
@@ -11,8 +14,10 @@ export const load: PageServerLoad = async () => {
 		}
 	});
 
+	const newProjectForm = await superValidate(arktype(newProject, { defaults: { name: '' } }));
+
 	return {
 		projects,
-		user
+		newProjectForm
 	};
 };
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,42 @@
 <script lang="ts">
+	import { Dialog, Label, Separator } from 'bits-ui';
+	import { X } from 'lucide-svelte';
 	let { data } = $props();
-
-	console.log(data);
 </script>
 
-<h1>hi</h1>
+<Dialog.Root open={true}>
+	<Dialog.Trigger class="btn preset-filled">New API key</Dialog.Trigger>
+	<Dialog.Portal>
+		<Dialog.Overlay
+			class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80"
+		/>
+		<Dialog.Content
+			class="rounded-container bg-surface-50-950 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 border-surface-300-700 fixed top-[50%] left-[50%] z-50 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] border p-5 shadow-2xl outline-hidden sm:max-w-[490px] md:w-full"
+		>
+			<Dialog.Title class="h3">Create Project</Dialog.Title>
+			<Dialog.Close
+				class="absolute top-5 right-5 rounded-md focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden active:scale-[0.98]"
+			>
+				<div>
+					<X class="text-surface-950-50 size-5" />
+					<span class="sr-only">Close</span>
+				</div>
+			</Dialog.Close>
+			<Separator.Root class="bg-surface-300-700 mt-2 mb-4 block h-px" />
+			<div class="flex flex-col items-start gap-1">
+				<Label.Root for="name" class="text-sm font-medium">Project Name</Label.Root>
+				<div class="relative w-full">
+					<input
+						id="name"
+						class="input placeholder:text-surface-700-300"
+						placeholder="e.g. Minecraft Base"
+						name="name"
+					/>
+				</div>
+			</div>
+			<div class="flex w-full justify-end">
+				<Dialog.Close class="btn preset-filled mt-4">Save</Dialog.Close>
+			</div>
+		</Dialog.Content>
+	</Dialog.Portal>
+</Dialog.Root>
