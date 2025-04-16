b8032cb0c8bdd8077f6f24404d0bd756715c6372
Neville Brem <contact@nevillebrem.com>
2025-04-16 12:48:39 +0200

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,12 @@
 	let { children } = $props();
 </script>
 
+<nav class="flex items-baseline justify-between gap-4 px-8 py-4">
+	<a href="/" class="text-5xl font-bold">D</a>
+	<div>
+		<a href="/projects" class="text-xl">Projects</a>
+	</div>
+</nav>
 <main class="m-8">
 	{@render children()}
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,17 @@
 	const { projects, user } = $derived(data);
 </script>
 
-<h3 class="h3 mb-2">Hi, {user.name}</h3>
+<h3 class="mb-2 text-xl italic">Hi, {user.name}</h3>
 <h1 class="h1 mb-4">Overview:</h1>
-<a class="anchor" href="/projects">See all projects</a>
 <div class="mt-4">
-	{#each projects as project}
-		<a href="/project/{project.id}">{project.name}</a>
-	{:else}
-		no projects...
-	{/each}
+	<ul>
+		{#each projects as project}
+			<li>
+				<a href="/projects/{project.id}">{project.name}</a>
+			</li>
+		{:else}
+			no projects...
+		{/each}
+	</ul>
+	<a class="anchor" href="/projects">See all projects</a>
 </div>
